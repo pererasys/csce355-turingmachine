9c0b5d945c668e07ec4f9f9c295ac365faa43b0b
Andrew Perera <pererasys@gmail.com>
2020-11-09 02:40:25 -0500

    refactor

--- palindrome.py
+++ palindrome.py
@@ -11,18 +11,19 @@ from turingmachine import TuringMachine
 
 parser = argparse.ArgumentParser()
 
-def main(topology, tape):
-    tm = TuringMachine()
-    tm.readTopology(topology)
+
+def main(which, topology, tape):
+    tm = TuringMachine(which, function=topology)
     tm.execute(tape)
 
 
 if __name__ == "__main__":
-    parser.add_argument('-i', '--input', dest='input', help='File name of the input file.', required=True)
-    parser.add_argument('-t', '--tape', dest='tape', help='File name of the tape.', required=True)
+    parser.add_argument('-w', '--which', dest='which',
+                        help='Prefix for all states.', required=True)
+    parser.add_argument('-i', '--input', dest='input',
+                        help='File name of the input file.', required=True)
+    parser.add_argument('-t', '--tape', dest='tape',
+                        help='File name of the tape.', required=True)
     options = parser.parse_args()
 
-    main(topology=options.input, tape=options.tape)
-
-
-
+    main(which=options.which, topology=options.input, tape=options.tape)
--- turingmachine.py
+++ turingmachine.py
@@ -10,21 +10,39 @@ import json
 
 class TuringMachine(object):
 
-    def __init__(self):
-        self._position = 1
-        self._topology = {}
-        self._current_state = None
+    def __init__(self, prefix="", function=None):
+        if not function:
+            raise Exception("You must provide a function for the TM.")
 
-    def readTopology(self, file):
-        file = open(file)
+        self._prefix = prefix
+        self._map = {}
+
+        self._describe_function(function)
+
+    def _is_final(self, state):
+        '''
+
+        Returns whether or not the state provided is a final state
+
+        @param state - str
+
+        '''
+
+        return state.split(self._prefix)[1] == "FINAL"
+
+    def _describe_function(self, filename):
+        '''
+
+        Creates a mapping for the given file-based description
+
+        @param filename - str
+
+        '''
+
+        file = open(filename)
         lines = file.readlines()
         for line in lines:
-            self._position = 1
             steps = line.split()
-
-            if line == lines[0]:
-                self._start_state = steps[0]
-                self._current_state = steps[0]
 
             if len(steps) > 0:
                 description = {
@@ -34,39 +52,41 @@ class TuringMachine(object):
                         "direction": steps[4]
                     }
                 }
-                self._topology.update(description)
+                self._map.update(description)
 
         file.close()
 
-    def parse_tape(self, tape):
-        if self._position > len(tape) - 1 or "FINAL" in self._current_state:
-            return tape
+    def parse_tape(self, tape, position, state):
+        '''
 
-        path = None
-        
-        path = self._topology[f'{self._current_state}<-{tape[self._position]}']
+        Parses the tape by recursively going through the steps until
+        it reaches the end of the tape or is in a final state.
+
+        @param tape - list(str)
+        @param position - int
+        @param state - str
+
+        '''
+
+        if self._is_final(state):
+            return "ACCEPTED", tape
+
+        path = self._map[f'{state}<-{tape[position]}']
 
         direction = path['direction']
         next_path = path['next']
-        tape[self._position] = str(path['write'])
+        tape[position] = str(path['write'])
 
-        if (self._position <= len(tape) - 1 or direction == "LEFT"):
-            self._current_state = next_path
-            if direction == 'LEFT':
-                self._position -= 1
-            else:
-                self._position += 1
+        if (position <= len(tape) - 1 or direction == "LEFT"):
+            position += 1 if direction == 'RIGHT' else -1
+            return self.parse_tape(tape=tape, position=position, state=next_path)
 
-            return self.parse_tape(tape)
-
-        return tape
+        return "NOT ACCEPTED", tape
 
     def execute(self, file):
         file = open(file)
         tapes = file.readlines()
         for tape in tapes:
-            self._position = 1
-            self._current_state = self._start_state
             tape = list(tape.strip())
-            output = self.parse_tape(tape)
-            print(''.join(output))
+            status, output = self.parse_tape(tape, position=1, state=f'{self._prefix}START')
+            print(f"{status} - {''.join(output)}")
